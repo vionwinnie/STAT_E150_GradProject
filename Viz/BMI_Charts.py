98f265c755e64634c5cf9840e3ee74eafd1f094d
vionwinnie <myeung@middlebury.edu>
2016-12-07 16:50:16 +0800

    Update BMI_Charts.py

--- Viz/BMI_Charts.py
+++ Viz/BMI_Charts.py
@@ -1,4 +1,4 @@
-#Reading the file into Python
+
 import seaborn as sns
 import pandas as pd
 tips = pd.read_csv(r"P:\Coursera\BMI\20161207_testWrite3_mostRecent_trim_grouped.csv")
@@ -8,32 +8,31 @@ tips = pd.read_csv(r"P:\Coursera\BMI\20161207_testWrite3_mostRecent_trim_grouped
 #
 #tips.replace({"Continent": di})
 
-#setting the color for different series 
-flatui = ["#95a5a6",  "#F781D8","#3498db"]
+flatui = ["#F781D8","#3498db","#95a5a6"]
 sns.set_palette(flatui)
-
-#setting the size of the plot, size of the font 
 sns.set_context("notebook", font_scale=1, rc={"lines.linewidth": 2})
 plt.figure(figsize=(6, 4))
-
-ax = sns.pointplot(y="Continent", x="MeanBMI_BothSexes",hue="Sex", data=tips, join=False, legend=False)
-
-#adding the legend to the right side of the plot
+ax = sns.pointplot(y="Continent", x="MeanBMI_BothSexes",hue="Sex", data=tips, join=False, legend=False, hue_order=['Female','Male','Mixed'])
 ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 
-#changing the size of the second data series 
-points = ax.collections[1]
+#changing the size of the first data series 
+points = ax.collections[0]
 size = points.get_sizes().item()
-new_sizes = [size * .01 if name.get_text() != "Fri" else size for name in ax.get_yticklabels()]
+new_sizes = [size * .01]
 points.set_sizes(new_sizes)
 
 #changing the size of the third data series
-points2 = ax.collections[2]
+points2 = ax.collections[1]
 size2 = points2.get_sizes().item()
-new_sizes2 = [size2 * .01 if name.get_text() != "Fri" else size for name in ax.get_yticklabels()]
+new_sizes2 = [size2 * .01 ]
 points2.set_sizes(new_sizes2)
 
-#rename axes
+#changing the size of the third data series
+points0 = ax.collections[2]
+size0 = points0.get_sizes().item()
+new_sizes0= [size0 * 1.5 ]
+points0.set_sizes(new_sizes0)
+
 ax.set(xlabel='Mean BMI', ylabel='Continent')
 
